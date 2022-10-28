c902562af6e19b6ad670b3f137164f4635eae4ca
Aduragbemi <aduragbemioduntan@gmail.com>
2022-10-28 11:34:25 +0100

    zuri week 5 python task, a continuation of week 4 task

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,19 @@
+from turtle import onclick
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=225)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=225)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+class Lyric(models.Model):
+    content = models.TextField(max_length=1000)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
